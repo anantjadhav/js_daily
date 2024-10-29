df85a38f46d9ef0a7ad57385ce9879197675ff9d
Anant Jadhav <anant.jadhav6@gmail.com>
2024-10-29 09:48:05 +0530

    All changes saved

---
+++ 28_Oct/script_28_10.js
@@ -0,0 +1,60 @@
+
+
+/* function greet(name) {
+    document.getElementById("greet").textContent = "Hello " + name;    
+}
+
+greet("Anant Jadhav") */
+
+
+let name = "Anant Jadhav";
+let age = 25;
+let occupation = "Web Developer";
+
+console.log(name);
+console.log(age);
+console.log(occupation);
+
+
+
+let message = 'Anant Jadhav';
+
+message = "Jadhav Anant!"
+
+console.log(message);
+
+if (true) {
+    var myInfo = 'abc';
+}
+
+console.log(myInfo);
+
+
+// concatination two strings
+
+let concateStrings = "Hello Friends";
+
+console.log("My message is " + concateStrings);
+
+
+let studentName = "Anant";
+let topic = 'Javascript';
+
+/* console.log(studentName + " is learning " + topic + " today."); */
+
+console.log(`${studentName} is learning ${topic} today.`);
+
+
+let unk = undefined;
+let nulle = null;
+
+console.log(typeof unk);
+console.log(typeof nulle);
+
+let x = "7";
+let y = 5;
+
+x = Number(x);
+
+console.log(x + y);
+
